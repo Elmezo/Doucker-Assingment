fadc2cf10a04ff83220bf5653340d08c0e3f4239
Abdelrahman Alaa Eldeen Ezz Eldeen <36506058+Elmezo@users.noreply.github.com>
2021-05-04 04:52:08 +0200

    Add files via upload

---
+++ Docker/TheCode.py
@@ -0,0 +1,27 @@
+file = open('Beyond the Wall of Sleep.txt', encoding="utf8")
+Beyond= file.read()
+file2 = open('Pride and Prejudice.txt', encoding="utf8")
+Pride= file2.read()
+Beyond=Beyond.lower().split()
+Pride=Pride.lower().split()
+intersection = set(Beyond) & set(Pride)
+words={'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"}
+
+for x in words:
+    intersection.discard(x)
+intersection = str(intersection)
+import re
+new_intersection = re.sub(r'[^\w\s]','',intersection)
+def unique_list(l):
+    ulist = []
+    [ulist.append(x) for x in l if x not in ulist]
+    return ulist
+
+new_intersection=' '.join(unique_list(new_intersection.split()))
+file.close()
+file2.close()
+word_list = new_intersection.split()
+number_of_words = len(word_list)
+print('The number of word = ',number_of_words)
+print('The word is : ')
+new_intersection
